f207060e4eaae50b936f2096b6fcec92e4bb35c6
siro <mueller.siro@gmail.com>
2019-05-13 13:50:56 +0200

    bug fixes

--- PRG2_gruppenarbeit01/src/Bankomatprogramm.java
+++ PRG2_gruppenarbeit01/src/Bankomatprogramm.java
@@ -10,9 +10,10 @@
  *  - Konten können von der Kontenliste gelöscht werden
  *  
  * Wie Testen:
+ * Pin zum Testkonto: 7777
  * Es existieren Kunden und Kontos zu finden in der jeweiligen DAO-Klasse (KundeDAO bzw. KontoDAO).
  * Es wird angenommen, dass die Kontonummer auf der Bankkarte gespeichert ist.
- * Beim Start des Programms mit der Klasse "Bankomatprogramm" kann man das Konto auswählen, auf welches zugegriffen werden soll
+ * Vor dem Start des Programms mit der Klasse "Bankomatprogramm" muss das gewünschte Konto in der Source ausgewählt werden
  * 
  * Gruppenarbeit 01 - PRG2
  * Klasse 2o
